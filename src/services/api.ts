f4af3cec66c8f724e979ed9f49d3bf6cfba61cf5
dhiyadaffaa16 <dhiyadaffaa16@gmail.com>
2025-12-19 14:31:18 +0700

    change api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3333';
+const API_BASE_URL = 'https://hows-the-air-40eve85ik-daffa-dhiya-ulhaqs-projects.vercel.app/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
